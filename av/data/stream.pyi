765462772395fee9a123c51fc1be6dc19a970349
laggykiller <chaudominic2@gmail.com>
2024-03-04 10:32:12 +0800

    Fix literal typing

--- av/data/stream.pyi
+++ av/data/stream.pyi
@@ -1,9 +1,11 @@
+from typing import Literal
+
 from av.frame import Frame
 from av.packet import Packet
 from av.stream import Stream
 
 class DataStream(Stream):
-    type = "data"
+    type: Literal["data"]
 
     def encode(self, frame: Frame | None = None) -> list[Packet]: ...
     def decode(self, packet: Packet | None = None, count: int = 0) -> list[Frame]: ...
